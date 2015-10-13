c03c4ec7f9e99981d0eccb4b69cb7da781a7db92
williamtavares <williamtavares@users.noreply.github.com>
2015-10-13 17:19:15 -0400

    -Refactored (Numpad layout no styling)

--- numpad-layout/numpad-layout.ino
+++ numpad-layout/numpad-layout.ino
@@ -1,4 +1,17 @@
-//By William Tavares
+/*
+ * Small Program to Simulate a Numpad using a 2.4" TFT Touchscreen
+ * Program does not act as an USB HID Device, for reference purposes only
+ * Tested on Arduino UNO Only and 0x9341
+ * By William Tavares
+ * 
+ * Note:
+ * This version has no styling or numbers displayed,
+ * if you want the complete version get the "numpad" program
+ * from my Github https://github.com/williamtavares/Arduino-Uno-NumPad
+ * 
+ * Open the Serial Monitor to see the program running
+ * Enjoy!
+*/
 
 #include <Adafruit_GFX.h>
 #include <TouchScreen.h>
@@ -9,10 +22,10 @@
 #define YM 6   // can be a digital pin
 #define XP 7   // can be a digital pin
 
-short TS_MINX = 150;
-short TS_MINY = 120;
-short TS_MAXX = 850;
-short TS_MAXY = 891; 
+#define TS_MINX 150
+#define TS_MINY 120
+#define TS_MAXX 850
+#define TS_MAXY 891
 
 //SPI Communication
 #define LCD_CS A3
@@ -28,8 +41,6 @@ short TS_MAXY = 891;
 #define RED     0xF800
 #define GREEN   0x07E0
 #define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
 
 #define MINPRESSURE 1
 #define MAXPRESSURE 1000
@@ -79,102 +90,95 @@ void setup() {
 
   tft.reset();  
   uint16_t identifier = tft.readID();
-  Serial.println(identifier, HEX);
   tft.begin(identifier);
 
+  //Background color
   tft.fillScreen(BLACK);
 
-  //(initial x,initial y,width,height,color)
-  //First Row
-  tft.drawRect(leftColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
-  tft.drawRect(midColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
-  tft.drawRect(rightColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
-
-  //Second Row
-  tft.drawRect(leftColPositionX, secondRow + verticalAlign, BOXSIZE, BOXSIZE, CYAN);
-  tft.drawRect(midColPositionX, secondRow + verticalAlign, BOXSIZE, BOXSIZE, CYAN);
-  tft.drawRect(rightColPositionX, secondRow + verticalAlign, BOXSIZE, BOXSIZE, CYAN);
-
-  //Third Row
-  tft.drawRect(leftColPositionX, thirdRow + verticalAlign, BOXSIZE, BOXSIZE, GREEN);
-  tft.drawRect(midColPositionX, thirdRow + verticalAlign, BOXSIZE, BOXSIZE, GREEN);
-  tft.drawRect(rightColPositionX, thirdRow + verticalAlign, BOXSIZE, BOXSIZE, GREEN);
-
-  //Fourth Row
-  tft.drawRect(leftColPositionX, fourthRow + verticalAlign, (BOXSIZE*2)+padding, BOXSIZE, RED);
-  tft.drawRect(rightColPositionX, fourthRow + verticalAlign, BOXSIZE, BOXSIZE, RED);
+  createButtons();
+  Serial.println(F("Press any button on the TFT screen: "));
 }
 
 void loop() {
   retrieveTouch();
+  int boxHeightRow1 = verticalAlign + BOXSIZE;
+  int boxHeightRow2 = secondRow + BOXSIZE;
+  int boxHeightRow3 = thirdRow + BOXSIZE;
+  int boxHeightRow4 = fourthRow + BOXSIZE;
 
   if(Z > MINPRESSURE && Z < MAXPRESSURE){
     //Check if element clicked is in left column
     if(X > leftColPositionX && X < (leftColPositionX+BOXSIZE)){
       //Check if element clicked is in row 1
-      if(Y > verticalAlign && Y < (verticalAlign + BOXSIZE)){
-           Serial.println("1");
-           delay(150); 
-      }
-      //Check if element clicked is in row 2
-      else if(Y > verticalAlign && Y < (secondRow + BOXSIZE)){
-           Serial.println("4");
-           delay(150); 
-      }
-      //Check if element clicked is in row 3
-      else if(Y > verticalAlign && Y < (thirdRow + BOXSIZE)){
-           Serial.println("7");
-           delay(150); 
-      }
-      //Check if element clicked is in row 4
-      else if(Y > verticalAlign && Y < (fourthRow + BOXSIZE)){
-           Serial.println("0");
-           delay(150); 
+      if(Y > verticalAlign){
+        if(Y < boxHeightRow1){
+             Serial.println("1");
+             delay(150); 
+        }
+        //Check if element clicked is in row 2
+        else if(Y < boxHeightRow2){
+             Serial.println("4");
+             delay(150); 
+        }
+        //Check if element clicked is in row 3
+        else if(Y < boxHeightRow3){
+             Serial.println("7");
+             delay(150); 
+        }
+        //Check if element clicked is in row 4
+        else if(Y < boxHeightRow4){
+             Serial.println("0");
+             delay(150); 
+        }        
       }
        //Check if element clicked is in mid column
     } else if (X > midColPositionX && X < (midColPositionX+BOXSIZE)){
       //Check if element clicked is in row 1
-      if(Y > verticalAlign && Y < (verticalAlign + BOXSIZE)){
-           Serial.println("2");
-           delay(150); 
-      }
-      //Check if element clicked is in row 2
-      else if(Y > verticalAlign && Y < (secondRow + BOXSIZE)){
-           Serial.println("5");
-           delay(150); 
-      }
-      //Check if element clicked is in row 3
-      else if(Y > verticalAlign && Y < (thirdRow + BOXSIZE)){
-           Serial.println("8");
-           delay(150); 
-      }
-      //Check if element clicked is in row 4
-      else if(Y > verticalAlign && Y < (fourthRow + BOXSIZE)){
-           Serial.println("0");
-           delay(150); 
-      }
+        if(Y > verticalAlign){
+          if(Y < boxHeightRow1){
+               Serial.println("2");
+               delay(150); 
+          }
+          //Check if element clicked is in row 2
+          else if(Y < boxHeightRow2){
+               Serial.println("5");
+               delay(150); 
+          }
+          //Check if element clicked is in row 3
+          else if(Y < boxHeightRow3){
+               Serial.println("8");
+               delay(150); 
+          }
+          //Check if element clicked is in row 4
+          else if(Y < boxHeightRow4){
+               Serial.println("0");
+               delay(150); 
+          }      
+        }
       //Check if element clicked is in third column
     } else if (X > rightColPositionX && X < (rightColPositionX+BOXSIZE)){
-      //Check if element clicked is in row 1
-      if(Y > verticalAlign && Y < (verticalAlign + BOXSIZE)){
-           Serial.println("3");
-           delay(150); 
-      }
-      //Check if element clicked is in row 2
-      else if(Y > verticalAlign && Y < (secondRow + BOXSIZE)){
-           Serial.println("6");
-           delay(150); 
-      }
-      //Check if element clicked is in row 3
-      else if(Y > verticalAlign && Y < (thirdRow + BOXSIZE)){
-           Serial.println("9");
-           delay(150); 
-      }
-      //Check if element clicked is in row 3
-      else if(Y > verticalAlign && Y < (fourthRow + BOXSIZE)){
-           Serial.println(".");
-           delay(150); 
-      }
+        if(Y > verticalAlign){
+          //Check if element clicked is in row 1
+          if(Y < boxHeightRow1){
+               Serial.println("3");
+               delay(150); 
+          }
+          //Check if element clicked is in row 2
+          else if(Y < boxHeightRow2){
+               Serial.println("6");
+               delay(150); 
+          }
+          //Check if element clicked is in row 3
+          else if(Y < boxHeightRow3){
+               Serial.println("9");
+               delay(150); 
+          }
+          //Check if element clicked is in row 3
+          else if(Y < boxHeightRow4){
+               Serial.println(".");
+               delay(150); 
+          }        
+        }
     }
   }
 }
@@ -193,4 +197,32 @@ void retrieveTouch()
     X = map(p.x, TS_MAXX, TS_MINX, 0, tft.width());
     Y = map(p.y, TS_MAXY, TS_MINY, 0, tft.height());
     Z = p.z;
+}
+
+void createButtons(){
+  //(initial x,initial y,width,height,color)
+  double secondRowVertialAlign = secondRow + verticalAlign;
+  double thirdRowVertialAlign = thirdRow + verticalAlign;
+  double fourthRowVertialAlign = fourthRow + verticalAlign;
+
+  /***Draw squares with specified dimensions and position (No Fill)***/
+  //(initial x,initial y,width,height,color)
+  //First Row
+  tft.drawRect(leftColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
+  tft.drawRect(midColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
+  tft.drawRect(rightColPositionX, verticalAlign, BOXSIZE, BOXSIZE, WHITE);
+
+  //Second Row
+  tft.drawRect(leftColPositionX, secondRowVertialAlign, BOXSIZE, BOXSIZE, CYAN);
+  tft.drawRect(midColPositionX, secondRowVertialAlign, BOXSIZE, BOXSIZE, CYAN);
+  tft.drawRect(rightColPositionX, secondRowVertialAlign, BOXSIZE, BOXSIZE, CYAN);
+
+  //Third Row
+  tft.drawRect(leftColPositionX, thirdRowVertialAlign, BOXSIZE, BOXSIZE, GREEN);
+  tft.drawRect(midColPositionX, thirdRowVertialAlign, BOXSIZE, BOXSIZE, GREEN);
+  tft.drawRect(rightColPositionX, thirdRowVertialAlign, BOXSIZE, BOXSIZE, GREEN);
+
+  //Fourth Row
+  tft.drawRect(leftColPositionX, fourthRowVertialAlign, (BOXSIZE*2)+padding, BOXSIZE, RED);
+  tft.drawRect(rightColPositionX, fourthRowVertialAlign, BOXSIZE, BOXSIZE, RED);
 }
